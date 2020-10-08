71b27ddb26d14d3c78ba7858493ccfec89441fae
AgenChicken24 <64702468+AgenChicken24@users.noreply.github.com>
2020-10-09 01:33:42 +0300

    Update index.js

--- html/index.js
+++ html/index.js
@@ -8,30 +8,30 @@ $(function() {
         let paused = data.paused
         if (cops == "red") {
             document.getElementById('cops').style.textShadow = "2px 0 0 #FF0000, 0 -2px 0 #FF0000, 0 2px 0 #FF0000, -2px 0 0 #FF0000";
-        } else if (cops = "yellow") {
+        } else if (cops == "yellow") {
             document.getElementById('cops').style.textShadow = "2px 0 0 #FFFF00, 0 -2px 0 #FFFF00, 0 2px 0 #FFFF00, -2px 0 0 #FFFF00";
-        } else if (cops = "green") {
+        } else if (cops == "green") {
             document.getElementById('cops').style.textShadow = "2px 0 0 #008000, 0 -2px 0 #008000, 0 2px 0 #008000, -2px 0 0 #008000";
         }
         if (emc == "red") {
             document.getElementById('ambulance').style.textShadow = "2px 0 0 #FF0000, 0 -2px 0 #FF0000, 0 2px 0 #FF0000, -2px 0 0 #FF0000";
-        } else if (emc = "yellow") {
+        } else if (emc == "yellow") {
             document.getElementById('ambulance').style.textShadow = "2px 0 0 #FFFF00, 0 -2px 0 #FFFF00, 0 2px 0 #FFFF00, -2px 0 0 #FFFF00";
-        } else if (emc = "green") {
+        } else if (emc == "green") {
             document.getElementById('ambulance').style.textShadow = "2px 0 0 #008000, 0 -2px 0 #008000, 0 2px 0 #008000, -2px 0 0 #008000";
         }
         if (mech == "red") {
             document.getElementById('mechanic').style.textShadow = "2px 0 0 #FF0000, 0 -2px 0 #FF0000, 0 2px 0 #FF0000, -2px 0 0 #FF0000";
-        } else if (mech = "yellow") {
+        } else if (mech == "yellow") {
             document.getElementById('mechanic').style.textShadow = "2px 0 0 #FFFF00, 0 -2px 0 #FFFF00, 0 2px 0 #FFFF00, -2px 0 0 #FFFF00";
-        } else if (mech = "green") {
+        } else if (mech == "green") {
             document.getElementById('mechanic').style.textShadow = "2px 0 0 #008000, 0 -2px 0 #008000, 0 2px 0 #008000, -2px 0 0 #008000";
         }
         if (taxi == "red") {
             document.getElementById('taxi').style.textShadow = "2px 0 0 #FF0000, 0 -2px 0 #FF0000, 0 2px 0 #FF0000, -2px 0 0 #FF0000";
-        } else if (taxi = "yellow") {
+        } else if (taxi == "yellow") {
             document.getElementById('taxi').style.textShadow = "2px 0 0 #FFFF00, 0 -2px 0 #FFFF00, 0 2px 0 #FFFF00, -2px 0 0 #FFFF00";
-        } else if (taxi = "green") {
+        } else if (taxi == "green") {
             document.getElementById('taxi').style.textShadow = "2px 0 0 #008000, 0 -2px 0 #008000, 0 2px 0 #008000, -2px 0 0 #008000";
         }
 
